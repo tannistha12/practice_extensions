a650c8b8f15d66bbf69333478760c47a455387fb
Tannistha Chattapadhyay <164680832+tannistha12@users.noreply.github.com>
2026-06-14 16:13:02 +0530

    Add files via upload

---
+++ reading_time/scripts/content.js
@@ -0,0 +1,44 @@
+function renderReadingTime() {
+  // if no article element is found, return
+  if (!article) {
+    return;
+  }
+
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g;
+  const words = text.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200); // Assuming an average reading speed of 200 words per minute
+  const badge = document.createElement("p");
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+renderReadingTime(document.querySelector("article"));
+
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    // If a new article was added.
+    for (const node of mutation.addedNodes) {
+      if (node instanceof Element && node.tagName === "ARTICLE") {
+        // Render the reading time for this particular article.
+        renderReadingTime(node);
+      }
+    }
+  }
+});
+
+// https://developer.chrome.com/ is a SPA (Single Page Application) so can
+// update the address bar and render new content without reloading. Our content
+// script won't be reinjected when this happens, so we need to watch for
+// changes to the content.
+observer.observe(document.querySelector("devsite-content"), {
+  childList: true,
+});
